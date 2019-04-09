7c524a6e44899750808b32ed71a87d1a13671105
Eziev Djamaldin <deziev@changers.team>
2019-04-09 23:23:31 +0300

    feat: Add access token into core service wrapper

---
+++ config/dev/services.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "core": {
+    "path": "http://localhost:3000/private/api",
+    "token": process.env.KICKER_SERVER_ACCESS_TOKEN
+  }
+}
--- src/services/wrappers/index.ts
+++ src/services/wrappers/index.ts
@@ -4,4 +4,4 @@ import { CoreServiceWrapper } from './CoreServiceWrapper';
 
 const config = configLoader.getConfig<ServicesConfig>('services');
 
-export const coreService = new CoreServiceWrapper(config.core.path);
+export const coreService = new CoreServiceWrapper(config.core.path, config.core.token);
